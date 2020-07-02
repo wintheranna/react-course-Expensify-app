0af7ee3080c657ef5c764c08ff68168c81cda80f
Anna Winther <annawiinther@gmail.com>
2020-07-02 14:03:02 +0200

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 
+
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -13,8 +14,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export {firebase, database as default};
+export {firebase, googleAuthProvider, database as default};
 
 // //child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
